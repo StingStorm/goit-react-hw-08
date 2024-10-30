2767180cc5cce9c75e76643d7d08719368adc562
StingStorm <alexkalganov18@gmail.com>
2024-10-30 19:57:54 +0200

    update titles

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className={css.header}>
       <p>Kalhanov Oleksandr</p>
-      <p>Homework-6</p>
+      <p>Homework-7</p>
       <p>React-108</p>
     </div>
   );
